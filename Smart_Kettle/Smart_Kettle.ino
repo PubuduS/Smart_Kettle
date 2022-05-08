1ca6f17e9e0bcd79d97bb2aa6b23ca4e9f4f100b
PubuduS <pubudusupun@gmail.com>
2022-05-08 14:19:53 -0500

    Add more functionality.
    Add keep warm functionality.
    Link up with power and keepwarm leds.
    Fix doxygen path to INO file.

--- Smart_Kettle/Smart_Kettle.ino
+++ Smart_Kettle/Smart_Kettle.ino
@@ -12,6 +12,7 @@ const uint8_t buzzer_Pin = 8;
 // Globals
 volatile bool heater_State = true;
 float temperature = 0.0;
+bool m_KeepWarmFlag = false;
 
 WriteToScreen m_Screen;
 TempSensor m_Sensor;
@@ -28,16 +29,16 @@ void setup()
   pinMode( led_Pin, OUTPUT );
   pinMode( buzzer_Pin, OUTPUT );
 
-  Serial.print("Connecting to WiFi…");
+  // Serial.print("Connecting to WiFi…");
   uint8_t status = WL_IDLE_STATUS;
   while (status != WL_CONNECTED) {
     status = WiFi.begin( WIFI_SSID, WIFI_PASSWORD );
-    Serial.print(".");
+    // Serial.print(".");
     delay(300);
   }
-  Serial.print(" IP: ");
+  //Serial.print(" IP: ");
   Serial.println( WiFi.localIP() );
-  Serial.println();
+  // Serial.println();
 
   Firebase.begin( FIREBASE_HOST, FIREBASE_AUTH, WIFI_SSID, WIFI_PASSWORD );
   Firebase.reconnectWiFi(true);
@@ -53,13 +54,35 @@ void setup()
 void loop() {
   // put your main code here, to run repeatedly:
 
+  if( Firebase.getBool( firebaseData, m_Path + "WarmFlag/" ) ) 
+  {
+    //Success, then read the payload value
 
+    //Make sure payload value returned from server is integer
+    //This prevent you to get garbage data
+    if( firebaseData.dataType() == "bool" ) 
+    {
+      m_KeepWarmFlag = firebaseData.boolData();
+      m_LedPanel.ControlKeepWarmLED( m_KeepWarmFlag );
+      // Serial.println( m_KeepWarmFlag );
+    }
+
+  } 
+  else 
+  {
+    //Failed, then print out the error detail
+    Serial.println( firebaseData.errorReason() );
+  }
+  
   if( heater_State == false )
   {
     m_Sensor.SensorState( true );
     m_Screen.ClearScreen();
     m_Screen.SendToScreen( "Heater OFF", (uint8_t)40, (uint8_t)30, 2 );
+    temperature = m_Sensor.GetTemperature();
+    FirebaseUpdateTemp( temperature );
     digitalWrite( led_Pin, LOW );
+    
     noTone( buzzer_Pin );
   }
   else
@@ -72,28 +95,41 @@ void loop() {
     HandleTemp();
     
   }
-
+  
+  m_LedPanel.ControlPowerLED( heater_State );
   FirebaseUpdateState( heater_State );
  
 }
 
-
+//! Change the state upon interrup.
 void ToggleState()
 {
   heater_State = !heater_State;  
 }
 
+//! Change the state based on temperature changes.
 void HandleTemp()
 {
-  if( temperature >= 70.0 )
+  if( temperature >= 95.0 )
   {
     digitalWrite( led_Pin, HIGH );
     tone( buzzer_Pin, 1000 );
     m_Sensor.SensorState( true );
     m_Screen.DrawCoffeeAnimation( 50, 32, 0);
   }
-  else
+  else if( m_KeepWarmFlag == true && temperature <= 88.0 && temperature >= 80.0 )
+  {
+    TurnHeaterOn();
+  }
+  else if( temperature < 30.0 )
   {    
+    TurnHeaterOn();
+  }
+}
+
+//! Turn on the heater.
+void TurnHeaterOn()
+{
     String temp = String(temperature, 2);
     String msg = temp +"C\n";
 
@@ -102,9 +138,9 @@ void HandleTemp()
     digitalWrite( led_Pin, LOW );
     noTone( buzzer_Pin );
     m_Sensor.SensorState( false ); 
-  }
 }
 
+//! Send the temperature data to database
 void FirebaseUpdateTemp( float temp )
 {
    if( Firebase.setFloat( firebaseData, m_Path + "Temp/", temp ) )
@@ -117,6 +153,7 @@ void FirebaseUpdateTemp( float temp )
    }
 }
 
+//! Send the state data to database
 void FirebaseUpdateState( bool state )
 {
    if( Firebase.setBool( firebaseData, m_Path + "State/", state ) )
